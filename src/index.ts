0544e03dacdc71d9b6898e37894ad03a45f273cf
Sajan <100612279+Epicslooyer@users.noreply.github.com>
2025-12-08 18:14:31 -0800

    Basic scaffold with controller/service/routes/middleware architecture

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,40 @@
+//Dependencies
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+
+//Routes
+import authroutes from "./routes/auth.routes";
+import todoroutes from "./routes/todo.routes";
+
+//Middleware to validate
+import { handler } from "./middleware/handler";
+import { validation } from "./middleware/validation";
 
 const app = express();
 const port = 3000;
-app.use(express.json());
 
+//Global Middlewares
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//API routes
+app.use('/auth', authroutes);
+app.use('/todo', todoroutes);
+
+//Error handling
+app.use(handler);
+app.use(validation);
+
+//Simple test
+app.get('/', (req, res) => {
+    res.send({status: 'ok'});
+});
+
+//Start server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
