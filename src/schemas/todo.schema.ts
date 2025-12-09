823d36bcd4dac33910826954b793ccadf219466d
Sajan <100612279+Epicslooyer@users.noreply.github.com>
2025-12-08 19:57:36 -0800

    Added todo validation middleware to protect updating and deleting

--- src/schemas/todo.schema.ts
+++ src/schemas/todo.schema.ts
@@ -1,16 +1,19 @@
 import { z } from "zod";
 
+//Create todo schema
 export const createTodoSchema = z.object({
     description: z.string().min(1),
     category: z.string().optional(),
 });
 
+//Update todo schema
 export const updateTodoSchema = z.object({
     description: z.string().min(1).optional(),
     category: z.string().optional(),
     completed: z.boolean().optional(),
 });
 
+//Filter todo schema
 export const filterTodoSchema = z.object({
     completed: z.boolean().optional(),
     category: z.string().optional(),
